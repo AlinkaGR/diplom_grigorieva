75761e7c84b88ecc09db5934f09a2d1260272158
Aleksandrov BO <aleksandrov.bo@gbksoft.com>
2021-06-22 23:06:30 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+
+# Laravel CRM Autoservice
+
+
+
+## Install with Docker
+
+- `docker-compose up -d`
+- `docker exec -it vue-starter /bin/bash`
+- `composer install`
+- `cp .env.example .env`
+- `php artisan key:generate`
+- `php artisan migrate`
+- `php artisan db:seed`
+- `php artisan passport:install`
+- Application http://localhost:8008/
+- Adminer for Database http://localhost:8080/
+- DBhost: yourIP:3307, user: root, Password: 123456
+
+
+## Unit Test
+
+#### run PHPUnit
+
+```bash
+# run PHPUnit all test cases
+vendor/bin/phpunit
+# or Feature test only
+vendor/bin/phpunit --testsuite Feature
+```
+
+#### Code Coverage Report
+
+```bash
+# reports is a directory name
+vendor/bin/phpunit --coverage-html reports/
+```
+A `reports` directory has been created for code coverage report. Open the dashboard.html.
---
+++ resources/js/components/Dashboard.vue
@@ -0,0 +1,212 @@
+<template>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row ">
+                <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box">
+                        <span class="info-box-icon bg-info elevation-1"><i class="fas fa-cog"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Autoservice Load</span>
+                            <span class="info-box-number">
+                            10
+                            <small>%</small>
+                            </span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box mb-3">
+                        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-thumbs-up"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Satisfy level</span>
+                            <span class="info-box-number">95 %</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+
+                <!-- fix for small devices only -->
+                <div class="clearfix hidden-md-up"></div>
+
+                <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box mb-3">
+                        <span class="info-box-icon bg-success elevation-1"><i class="fas fa-shopping-cart"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">Sales</span>
+                            <span class="info-box-number">1084</span>
+                        </div>
+                        <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+                <!-- /.col -->
+                <div class="col-12 col-sm-6 col-md-3">
+                    <div class="info-box mb-3">
+                        <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
+
+                        <div class="info-box-content">
+                            <span class="info-box-text">New Clients This Months</span>
+                            <span class="info-box-number">150</span>
+                        </div>
+                    <!-- /.info-box-content -->
+                    </div>
+                    <!-- /.info-box -->
+                </div>
+            <!-- /.col -->
+            </div>
+            <!-- /.row -->
+
+            <div class="row">
+                <!-- Left col -->
+                <div class="col-md-8">
+                    <!-- MAP & BOX PANE -->
+                    <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Latest Orders</h3>
+
+                        <div class="card-tools">
+                        <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                            <i class="fas fa-minus"></i>
+                        </button>
+                        <button type="button" class="btn btn-tool" data-card-widget="remove">
+                            <i class="fas fa-times"></i>
+                        </button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                        <table class="table m-0">
+                            <thead>
+                                <tr>
+                                    <th>Order ID</th>
+                                    <th>Client</th>
+                                    <th>Status</th>
+                                    <th>Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><a href="pages/examples/invoice.html">OR9842</a></td>
+                                    <td>John Doe</td>
+                                    <td><span class="badge badge-success">Working</span></td>
+                                    <td>
+                                        <div class="sparkbar" data-color="#00a65a" data-height="20">1000</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><a href="pages/examples/invoice.html">OR1848</a></td>
+                                    <td>Foo Bar</td>
+                                    <td><span class="badge badge-warning">Pending</span></td>
+                                    <td>
+                                        <div class="sparkbar" data-color="#f39c12" data-height="20">15000</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                                    <td>Mike Thompson</td>
+                                    <td><span class="badge badge-danger">Blocked</span></td>
+                                    <td>
+                                        <div class="sparkbar" data-color="#f56954" data-height="20">5000</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                                    <td>Steve Dry</td>
+                                    <td><span class="badge badge-info">Processing</span></td>
+                                    <td>
+                                        <div class="sparkbar" data-color="#00c0ef" data-height="20">900</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><a href="pages/examples/invoice.html">OR1848</a></td>
+                                    <td>Matt Tuck</td>
+                                    <td><span class="badge badge-warning">Pending</span></td>
+                                    <td>
+                                        <div class="sparkbar" data-color="#f39c12" data-height="20">30000</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><a href="pages/examples/invoice.html">OR7429</a></td>
+                                    <td>Luck Thompson</td>
+                                    <td><span class="badge badge-danger">Blocked</span></td>
+                                    <td>
+                                        <div class="sparkbar" data-color="#f56954" data-height="20">1000</div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        </div>
+                    </div>
+                <!-- /.card-body -->
+                </div>
+                <!-- /.card -->
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+
+            <div class="col-md-4">
+                <!-- Info Boxes Style 2 -->
+                <div class="info-box mb-3 bg-secondary">
+                <span class="info-box-icon"><i class="fas fa-tag"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Warehouse</span>
+                    <span class="info-box-number">15000</span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+                <div class="info-box mb-3 bg-success">
+                <span class="info-box-icon"><i class="far fa-heart"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Potential clients</span>
+                    <span class="info-box-number">58</span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+                <div class="info-box mb-3 bg-danger">
+                <span class="info-box-icon"><i class="fas fa-cloud-download-alt"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Income for month</span>
+                    <span class="info-box-number">114,381</span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+                <div class="info-box mb-3 bg-info">
+                <span class="info-box-icon"><i class="far fa-comment"></i></span>
+
+                <div class="info-box-content">
+                    <span class="info-box-text">Increased from last month</span>
+                    <span class="info-box-number">163,921</span>
+                </div>
+                <!-- /.info-box-content -->
+                </div>
+                <!-- /.info-box -->
+            </div>
+            <!-- /.col -->
+            </div>
+            <!-- /.row -->
+        </div><!--/. container-fluid -->
+    </section>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
